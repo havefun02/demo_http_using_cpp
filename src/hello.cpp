a07e6a9b67d260cf759a1120685adae26bd65d50
havefun02 <lapphan290802@gmail.com>
2023-02-19 12:52:03 +0700

    add hello

---
+++ src/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<sys/socket.h>
+#include<unistd.h>
+#define PORT 2323
+using namespace std;
+int main()
+{
+	cout<<"hello world";
+  	cout<<"i have been here!";
+	return 0;
+}
+
